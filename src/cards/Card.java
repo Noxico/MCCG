611f5ea1ddb233e94cabe30abb0d668ade0a8360
MartinG <martin.jgarcia.07@gmail.com>
2020-10-14 15:38:16 -0500

    chase branch
    wanted to create changes to chase brance

--- src/cards/Card.java
+++ src/cards/Card.java
@@ -12,7 +12,11 @@ public class Card
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
+<<<<<<< Updated upstream
 
+=======
+		System.out.println("I think I got ittt");
+>>>>>>> Stashed changes
 	}
 
 }
